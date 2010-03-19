ae02de4e721e4c6708d458eef84e8ffb167012f9
ckannan <kannan.chandrasekar@gmail.com>
2010-03-19 13:41:51 -0700

    updated to print ticket url

--- create-story.sh
+++ create-story.sh
@@ -24,7 +24,7 @@ formtoken=$( cat cookies.txt | grep trac_form_token | cut -f 7 | tr -d "\r" )
 
 # login; try to log in either form based or with HTTP authentication
 # make it in a single run; instead just doing both ways works - either one will work
-echo "Logging into $agilo"
+#echo "Logging into $agilo"
 # try logging login for form based login
 loginData="__FORM_TOKEN=$formtoken&user=$user&password=$pass"
 curl -s -b "$cookies" -c "$cookies" -d "$loginData" $agilo/login > /dev/null
@@ -35,13 +35,14 @@ curl -s -b "$cookies" -c "$cookies" --digest -u "$user:$pass"  $agilo/login  > /
 
 DATA="field_type=User+Story&field_summary=${summary}&field_reporter=${reporter}&field_description=${description}&field_keywords=${keywords}&field_rd_points=${rdpoints}&field_sprint=${sprint}&field_story_priority=${priority}&field_req_priority=1&field_owner=&field_status=new&submit=Create+ticket&__FORM_TOKEN=${formtoken}"
 
-curl -s -d "${DATA}" -b "$cookies" $agilo/newticket > /dev/null
-if [ $? -ne 0 ];
-then
-	echo "ERROR: Problem creating story"
+ticket_url=`curl -s -d "${DATA}" -i -b "$cookies" $agilo/newticket  | grep Location | grep -i ticket | awk '{print $2}'`
+if [ -n $ticket_url ];
+then    
+        echo "Story created = $ticket_url"
 else
-	echo "story created"
+        echo "ERROR: problem creating story"
 fi
+
 
 rm "$cookies"
 
